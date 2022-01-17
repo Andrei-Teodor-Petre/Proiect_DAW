0f12fe0f51d086f00cba1f8e45d29bf241687b53
Andrei <andreip@cymbiot.com>
2022-01-17 23:07:31 +0200

    Adding auth with jwt - wip

--- QuertyKey_Angular/src/app/app-routing.module.ts
+++ QuertyKey_Angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch: 'full', redirectTo: '/login'},
+  {path:'/login', component: LoginComponent, canActivate: [AuthGuard]},
+  {
+    path:'Keyboards',
+    canActivate: [KeyboardGuard],
+    canload: [KeyboardGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
