615ad3a72525b5b18a6d859b59dc056fd608d461
Andrei <andreip@cymbiot.com>
2022-01-19 23:01:36 +0200

    Updated data context. Created data models with scaffold. Removed redundant models.

--- QuertyKey_DAW/QuertyKey_DAW/DataModels/QuertyKey_DAWContext.cs
+++ QuertyKey_DAW/QuertyKey_DAW/DataModels/QuertyKey_DAWContext.cs
@@ -30,7 +30,7 @@ namespace QuertyKey_DAW.DataModels
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263. security LMAO
                 optionsBuilder.UseNpgsql("Server=localhost; Port=5433; Database=QuertyKey_DAW; Username=andrei; Password=1234;");
             }
         }
