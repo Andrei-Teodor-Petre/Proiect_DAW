e211f49a073c363a6006920692e3bbb87d335ac0
Andrei <andreip@cymbiot.com>
2022-01-19 23:16:14 +0200

    Updated solution files. Removed binaries form the repository

--- QuertyKey_DAW/QuertyKey_DAW/Program.cs
+++ QuertyKey_DAW/QuertyKey_DAW/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
-using QuertyKey_DAW.EF;
+using QuertyKey_DAW;
+using QuertyKey_DAW.DataModels;
+using QuertyKey_DAW.Repositories;
+using QuertyKey_DAW.UnitOfWork;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,10 +15,25 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-builder.Services.AddDbContext<DataContext>(
+builder.Services.AddDbContext<QuertyKey_DAWContext>(
     options =>
         options.UseNpgsql(builder.Configuration["ConnectionString"])
 );
+
+#region Repositories
+builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddTransient<AccessoryRepository>();
+builder.Services.AddTransient<DeskmatRepository>();
+builder.Services.AddTransient<KeyboardRepository>();
+builder.Services.AddTransient<KeycapRepository>();
+builder.Services.AddTransient<MerchRepository>();
+builder.Services.AddTransient<OrderRepository>();
+builder.Services.AddTransient<SpecialtyRepository>();
+builder.Services.AddTransient<SwitchRepository>();
+builder.Services.AddTransient<UserRepository>();
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
