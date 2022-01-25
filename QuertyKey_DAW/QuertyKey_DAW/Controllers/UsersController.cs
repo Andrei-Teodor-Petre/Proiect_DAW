0c95c10fc008a4cb29c39ffef60be0d475185f79
Andrei <andreip@cymbiot.com>
2022-01-25 21:35:19 +0200

    Implemented the User Controller

--- QuertyKey_DAW/QuertyKey_DAW/Controllers/UsersController.cs
+++ QuertyKey_DAW/QuertyKey_DAW/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using frontendModel = QuertyKey_DAW.FrontendModels;
+using dataModel = QuertyKey_DAW.DataModels;
+
 
 namespace QuertyKey_DAW.Controllers
 {
+    [ApiController]
+    [Route("/User")]
     public class UsersController : BaseController
     {
 
@@ -10,47 +15,109 @@ namespace QuertyKey_DAW.Controllers
 
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         [HttpGet]
-        [Route("List")]
+        [Route("/List")]
         public IActionResult GetListedItems()
         {
-
             var items = this._unitOfWork.Users.GetAll();
+            this._unitOfWork.Complete();
             return Ok();
             
         }
 
         [HttpPost]
-        [Route("Add")]
-        public IActionResult AddItem()
+        [Route("/Add")]
+        public IActionResult AddItem(frontendModel.User user)
         {
-            return Ok();
+            try
+            {
+                if(this._unitOfWork.Users.Find(usr => usr.Username == user.Username).Any())
+                {
+                    return BadRequest("Username already in use");
+                }
+                var newUser = new dataModel.User();
+                newUser.Name = user.Name;
+                newUser.Role = 0;
+                newUser.Username = user.Username;
+                newUser.Password = user.Password;
+                newUser.CreatedOn = DateOnly.Parse(DateTime.Now.ToString());
+                newUser.Age = user.Age;
+
+                this._unitOfWork.Users.Add(newUser);
+
+                var addedUser = this._unitOfWork.Users.Find(user => user.Username == newUser.Username).FirstOrDefault();
+                return Ok(addedUser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         [HttpPost]
-        [Route("Item")]
-        public IActionResult GetItem()
+        [Route("/Item")]
+        public IActionResult GetItem(frontendModel.User user)
         {
-            return Ok();
+            try
+            {
+                if (!this._unitOfWork.Users.Find(usr => usr.Username == user.Username).Any())
+                {
+                    return BadRequest("Cannot find the requested user");
+                }
+
+                var foundUser = this._unitOfWork.Users.Find(usr => user.Username == usr.Username).FirstOrDefault();
+                return Ok(foundUser);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
-        [Route("Update")]
-        public IActionResult UpdateItem()
+        [Route("/Update")]
+        public IActionResult UpdateItem(frontendModel.User user)
         {
-            return Ok();
+            try
+            {
+                var oldUser = this._unitOfWork.Users.Find(usr => user.Username == usr.Username).FirstOrDefault();
+
+                oldUser.Name = user.Name;
+                oldUser.Role = user.Role;
+                oldUser.Username = user.Username;
+                oldUser.Password = user.Password;
+                oldUser.Age = user.Age;
+
+
+                this._unitOfWork.Users.Update(oldUser);
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         [HttpDelete]
-        [Route("Remove")]
-        public IActionResult RemoveItem()
+        [Route("/Remove")]
+        public IActionResult RemoveItem(frontendModel.User user)
         {
-            return Ok();
+            try
+            {
+                var deleteItem = this._unitOfWork.Users.Find(usr => user.Username == usr.Username).FirstOrDefault();
+                
+                this._unitOfWork.Users.Remove(deleteItem);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
     }
 }
