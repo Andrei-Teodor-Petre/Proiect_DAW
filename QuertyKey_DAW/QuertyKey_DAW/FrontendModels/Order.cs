84484561e1218bb98054d8d4a5a897b86f7ac612
Andrei <andreip@cymbiot.com>
2022-01-26 21:58:49 +0200

    Updated properties names

--- QuertyKey_DAW/QuertyKey_DAW/FrontendModels/Order.cs
+++ QuertyKey_DAW/QuertyKey_DAW/FrontendModels/Order.cs
@@ -4,13 +4,13 @@
     {
         public Guid Id { get; set; }
         public DateTime CreatedOn { get; set; }
-        public List<Keyboard> keyboards { get; set; }
-        public List<Deskmat> deskmats { get; set; }
-        public List<Switch> switches { get; set; }
-        public List<Keycap> keycaps { get; set; }
-        public List<Merch> merch { get; set; }
-        public List<Accessory> accessories { get; set; }
-        public List<Specialty> specialties { get; set; }
+        public List<Keyboard> Keyboards { get; set; }
+        public List<Deskmat> Deskmats { get; set; }
+        public List<Switch> Switches { get; set; }
+        public List<Keycap> Keycaps { get; set; }
+        public List<Merch> Merches { get; set; }
+        public List<Accessory> Accessories { get; set; }
+        public List<Specialty> Specialties { get; set; }
         public decimal Price { get; set; }
     }
 }
